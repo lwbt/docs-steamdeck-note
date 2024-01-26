abfdb9b6e17ea324f0a36115e62af49884c44511
lwbt <benjaminosm@googlemail.com>
2024-01-27 00:52:39 +0100

    Delete windows_installation directory

--- windows_installation/mswindows_01powershell.ps1
+++
@@ -1,9 +0,0 @@
-# Get-Content mswindows_01powershell.ps1 | PowerShell.exe -noprofile -
-
-# !!! Run the command from the first line manually !!!
-
-# By default script execution is disabled, thus we need to enable it
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-# Using PowerShell directly navigate to the App Installer app in the store and update it
-Start-Process -FilePath ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1
